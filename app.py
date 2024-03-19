33238a42700a4f600eb14fe6ae84f52217b95773
mohamedelgeni <36715062+mohamedelgeni@users.noreply.github.com>
2024-03-19 21:45:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="💬 healthcare chatbot ")
+st.set_page_config(page_title="💬healthcare chatbot")
 
 # Replicate Credentials
 with st.sidebar:
     st.title('🦙💬 Llama 2 Chatbot')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
-        st.warning('Please enter your credentials!', icon='⚠️')
     else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
@@ -26,6 +26,7 @@ if "messages" not in st.session_state.keys():
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
+
 
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
