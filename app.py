0445d8ce37f30aad3d00421b56a0ab367c1dbd56
mohamedelgeni <36715062+mohamedelgeni@users.noreply.github.com>
2024-03-19 21:41:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="💬 healthcare chatbot ")
 
 # Replicate Credentials
 with st.sidebar:
@@ -13,9 +13,7 @@ with st.sidebar:
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
 # Store LLM generated responses
