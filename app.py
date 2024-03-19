89326726c550e1bbfbf41cc40c1a1ee400befc7b
mohamedelgeni <36715062+mohamedelgeni@users.noreply.github.com>
2024-03-19 21:39:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,6 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
-st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 # Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
